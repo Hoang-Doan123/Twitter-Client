e247de7cc62f194425dbcffbdbe38caf3db1c760
VitNguyen2003 <vietnq.ba12-192@st.usth.edu.vn>
2024-09-15 00:08:39 +0700

    Search Part

---
+++ TwitterSearch/app/src/main/java/vn/edu/usth/usth/twitterclient/SportFragment.java
@@ -0,0 +1,26 @@
+package vn.edu.usth.usth.twitterclient;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class SportFragment extends Fragment {
+
+    public SportFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sport, container, false);
+    }
+}
